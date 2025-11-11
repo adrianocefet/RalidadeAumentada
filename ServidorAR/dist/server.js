66eedcbc4e59bdd5cb6967b0044c368cc004d31e
adrianocefet <adrianocefet@hotmail.com>
2025-11-11 16:40:55 -0300

    servidor teste

---
+++ ServidorAR/dist/server.js
@@ -0,0 +1,6 @@
+// --- Servidor WebSocket Simples (server.js) ---
+// Este arquivo NÃO é executado no navegador.
+// Você precisa ter o Node.js instalado.
+//
+// 1. Salve este arquivo como 'server.js' na pasta do seu projeto.
+// 2
